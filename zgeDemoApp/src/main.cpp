3e49d0db0ae16e54ef5d04537b470cdc78a89c8b
Victor Sago <VictorSago01@gmail.com>
2018-01-10 16:53:14 +0100

    FindSDL2 cmake modules. SDL2 sanity check.

--- zgeDemoApp/src/main.cpp
+++ zgeDemoApp/src/main.cpp
@@ -3,9 +3,20 @@
 //
 
 #include <iostream>
+#include <SDL2/SDL.h>
 
 using namespace std;
 
 int main(int argc, char** argv) {
     cout << "Hello, My Game Engine!" << endl;
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        cout << "SDL_Init Error: " << SDL_GetError() << endl;
+        return EXIT_FAILURE;
+    } else {
+        cout << "SDL Initialized successfully!" << endl;
+    }
+
+    SDL_Quit();
+    return EXIT_SUCCESS;
 }

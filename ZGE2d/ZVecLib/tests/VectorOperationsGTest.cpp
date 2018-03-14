7a8cba2447204eb81d115f31bbb5de3b59aab9ca
Victor Sago <VictorSago01@gmail.com>
2018-03-14 13:57:15 +0100

    Tests for angle difference.

--- ZGE2d/ZVecLib/tests/VectorOperationsGTest.cpp
+++ ZGE2d/ZVecLib/tests/VectorOperationsGTest.cpp
@@ -10,9 +10,14 @@
 
 namespace {
 
-TEST_F(VectorOperationsGTest, angleBetweenTwoVecs) {
+TEST_F(VectorOperationsGTest, angleBetweenTwoVecs1) {
     std::cout << "GTest: Testing angle between vectors." << std::endl;
     ASSERT_DOUBLE_EQ(zvectors::PI_D/2, zvectors::Vec2dRec::angleDiff(vec11, vec12));
+}
+
+TEST_F(VectorOperationsGTest, angleBetweenTwoVecs2) {
+    std::cout << "GTest: Testing angle between vectors." << std::endl;
+    ASSERT_NEAR(zvectors::PI_D, zvectors::Vec2dRec::angleDiff(vec11, vec13), 0.0000001);
 }
 
 }
